514ffe3ce77bdc6aaa63d3c3b0960953e04b51c1
Sergio Guzman <hiserg98@gmail.com>
2019-05-06 19:59:02 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -19,12 +19,15 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  
+  num1 = num1 % num2
+  return num1
 end
 
 def square_root(num)
+  num = num *num
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  num 1 = num1 + ((num2 * num3 ) / num4 )
+  return num1
 end
